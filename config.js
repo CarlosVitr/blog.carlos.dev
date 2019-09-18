01591de8fc32183065c773d61de0f4b6306ca661
frencojobs <frenco.willian@gmail.com>
2019-09-18 11:08:18 +0630

    chore: initial commit

---
+++ config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  url: 'https://blog.frenco.codes',
+  pathPrefix: '/',
+  title: 'Blog by Frenco',
+  subtitle: 'words not dat good tho',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 5,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [{
+    label: '',
+    path: ''
+  }],
+  author: {
+    name: 'Frenco Jobs',
+    photo: '/photo.jpg',
+    bio: 'student at UIT, codes ocassionally',
+    contacts: {
+      email: 'me@frenco.codes',
+      facebook: '',
+      telegram: '',
+      twitter: 'https://twitter.com/frencojobs',
+      github: 'https://github.com/frencojobs',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: ''
+    }
+  }
+};
