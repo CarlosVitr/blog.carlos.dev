5612ef679f99ab02227f8ddc6e9beece57e4f41f
Frenco Jobs <frenco.willian@gmail.com>
2020-04-16 22:58:50 +0630

    Added darkmode-js and prettier

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,6 @@
-require("./src/assets/scss/init.scss");
-require("./static/css/prismjs/theme.min.css");
+require('./src/assets/scss/init.scss')
+require('./static/css/prismjs/theme.min.css')
+const Darkmode = require('darkmode-js')
+
+const darkmode = new Darkmode()
+darkmode.showWidget()
