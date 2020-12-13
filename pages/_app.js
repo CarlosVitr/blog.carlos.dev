9c1fd5c4336623d6402849eed4593fbf5141476e
Frenco <hey@frenco.dev>
2020-12-13 22:25:33 +0630

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
+import * as gtag from "@utils/gtag";
 
 import "@assets/main.css";
 
@@ -6,6 +9,18 @@ import "fontsource-ibm-plex-mono";
 import "fontsource-ibm-plex-mono/500.css";
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider defaultTheme="system" enableSystem={true} attribute="class">
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Head, Main, NextScript, Html } from "next/document";
+import { GA_TRACKING_ID } from "@utils/gtag";
 
 import { getSiteMetaData } from "@utils/helpers";
 
@@ -8,7 +9,25 @@ export default class MyDocument extends Document {
 
     return (
       <Html lang={siteMetadata.language}>
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
